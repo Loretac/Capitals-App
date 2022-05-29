08a2473f7caa24ac4955d1bcf5d669d4fe8df6b9
Loretac <LC@vt.edu>
2022-05-28 22:31:19 -0500

    Initial Commit

---
+++ Capitals-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capitals-App
+//
+//  Created by Chris Loreta on 5/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
