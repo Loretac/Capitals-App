9a63ed1e4374a9a8995ab28331c46615e8bbd955
Loretac <LC@vt.edu>
2022-05-30 15:27:21 -0500

    Set up tab bar view with additional view controllers

--- Capitals-App/Controller/QuizViewController.swift
+++ Capitals-App/Controller/QuizViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuizViewController: UIViewController {
 
     @IBOutlet weak var countryNameLabel: UILabel!
     @IBOutlet weak var answer1Button: UIButton!
