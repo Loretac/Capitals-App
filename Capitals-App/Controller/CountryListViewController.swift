b2c9fe72f8101937632f7d309725c2d930e07cf5
Loretac <LC@vt.edu>
2022-06-01 03:58:02 -0500

    Search bar dismisses upon tap, drag, and search button

--- Capitals-App/Controller/CountryListViewController.swift
+++ Capitals-App/Controller/CountryListViewController.swift
@@ -21,35 +21,39 @@ class CountryListViewController: UIViewController, UISearchBarDelegate {
         super.viewDidLoad()
 
         tableView.dataSource = self
-        searchBar.delegate=self
+        searchBar.delegate = self
 
-        
         countryManager.initData()
         listOfCountries = countryManager.getCountryList()
         filteredListOfCountries = listOfCountries
 
         tableView.register(UINib(nibName: "CountryCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
-
-        // Do any additional setup after loading the view.
     }
 
-    
-    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         filteredListOfCountries = []
-        
-        if(searchText == ""){
-            filteredListOfCountries=listOfCountries
+
+        if(searchText == "") {
+            filteredListOfCountries = listOfCountries
         }
-        
-        
-        for x in listOfCountries{
-            if x.name.lowercased().contains(searchText.lowercased()){
+
+
+        for x in listOfCountries {
+            if x.name.lowercased().contains(searchText.lowercased()) {
                 filteredListOfCountries.append(x)
             }
         }
-        
+
         self.tableView.reloadData()
+    }
+
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+
+    @IBAction func tapToHideKeyboard(_ sender: UITapGestureRecognizer) {
+        self.searchBar.resignFirstResponder()
     }
 
 }
@@ -62,15 +66,15 @@ extension CountryListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! CountryCell
         cell.countryNameLabel.text = filteredListOfCountries[indexPath.row].name
-        cell.capitalLabel.text =   filteredListOfCountries[indexPath.row].capital
+        cell.capitalLabel.text = filteredListOfCountries[indexPath.row].capital
         cell.flagImage.image = UIImage(named: "\(filteredListOfCountries[indexPath.row].iso_code.lowercased()).png")
-        
+
 //        cell.flagImage.layer.shadowColor = UIColor.black.cgColor
 //        cell.flagImage.layer.shadowOpacity = 0.2
 //        cell.flagImage.layer.shadowOffset = CGSize(width: 0, height: 0)
 //        cell.flagImage.layer.shadowRadius = 15.0
-        
-        
+
+
         return cell
     }
 
