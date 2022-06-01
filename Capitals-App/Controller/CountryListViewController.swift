675b2448ab90400d3db22b954b1ecb4ff28125eb
Loretac <LC@vt.edu>
2022-06-01 03:38:25 -0500

    Added functional search bar

--- Capitals-App/Controller/CountryListViewController.swift
+++ Capitals-App/Controller/CountryListViewController.swift
@@ -7,50 +7,70 @@
 
 import UIKit
 
-class CountryListViewController: UIViewController {
+class CountryListViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
 
+    @IBOutlet weak var searchBar: UISearchBar!
     var countryManager = CountryManager()
 
     var listOfCountries: [CountryModel] = []
+    var filteredListOfCountries: [CountryModel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
+        searchBar.delegate=self
 
+        
         countryManager.initData()
         listOfCountries = countryManager.getCountryList()
+        filteredListOfCountries = listOfCountries
 
         tableView.register(UINib(nibName: "CountryCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredListOfCountries = []
+        
+        if(searchText == ""){
+            filteredListOfCountries=listOfCountries
+        }
+        
+        
+        for x in listOfCountries{
+            if x.name.lowercased().contains(searchText.lowercased()){
+                filteredListOfCountries.append(x)
+            }
+        }
+        
+        self.tableView.reloadData()
     }
-    */
 
 }
 
 extension CountryListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listOfCountries.count
+        return filteredListOfCountries.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! CountryCell
-        cell.countryNameLabel.text = listOfCountries[indexPath.row].name
-        cell.capitalLabel.text =   listOfCountries[indexPath.row].capital
-        cell.flagImage.image = UIImage(named: "\(listOfCountries[indexPath.row].iso_code.lowercased()).png")
+        cell.countryNameLabel.text = filteredListOfCountries[indexPath.row].name
+        cell.capitalLabel.text =   filteredListOfCountries[indexPath.row].capital
+        cell.flagImage.image = UIImage(named: "\(filteredListOfCountries[indexPath.row].iso_code.lowercased()).png")
+        
+//        cell.flagImage.layer.shadowColor = UIColor.black.cgColor
+//        cell.flagImage.layer.shadowOpacity = 0.2
+//        cell.flagImage.layer.shadowOffset = CGSize(width: 0, height: 0)
+//        cell.flagImage.layer.shadowRadius = 15.0
+        
+        
         return cell
     }
 
