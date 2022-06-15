cdec07c64c4cef90cedabfb47be6cc8077b04d94
Loretac <LC@vt.edu>
2022-06-14 21:32:54 -0500

    Added settings page

---
+++ Capitals-App/Controller/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Capitals-App
+//
+//  Created by Chris Loreta on 6/14/22.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController{
+    
+}
